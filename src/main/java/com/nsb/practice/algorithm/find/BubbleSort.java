e8601ab27c62073c65719c643f1aa59e5c056251
dorae <nsb_2017@163.com>
2018-09-26 19:27:02 +0800

    bubbleSort

---
+++ src/main/java/com/nsb/practice/algorithm/find/BubbleSort.java
@@ -0,0 +1,35 @@
+package com.nsb.practice.algorithm.find;
+
+/**
+ * 冒泡排序
+ * 
+ * @author Dorae
+ *
+ */
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int[] nums = bubbleSort(Constants.INT_SEQ, 0, Constants.INT_SEQ.length);
+		for (int i : nums) {
+			System.out.print(i + ", ");
+		}
+	}
+
+	public static int[] bubbleSort(int[] nums, int start, int end) {
+		int flag = 0;
+		for (int i = start; i <= end; i++) {
+			for (int j = end; j > i; j--) {
+				if (nums[j] < nums[j - 1]) {// 交换
+					nums[j] = nums[j] ^ nums[j - 1];
+					nums[j - 1] = nums[j] ^ nums[j - 1];
+					nums[j] = nums[j] ^ nums[j - 1];
+					flag++;
+				}
+			}
+			if (flag == 0) {
+				break;
+			}
+		}
+		return nums;
+	}
+}
