72d925ba0f6f430a9a56e514646b4331cbcadc52
dorae <nsb_2017@163.com>
2018-09-26 15:25:02 +0800

    direct insert sort

---
+++ src/main/java/com/nsb/practice/algorithm/find/DirectInsertSort.java
@@ -0,0 +1,43 @@
+package com.nsb.practice.algorithm.find;
+
+/**
+ * 直接插入排序
+ * @author Dorae
+ *
+ */
+public class DirectInsertSort {
+
+	public static void main(String[] args) {
+		int[] num = directInsertSort(Constants.INT_SEQ, 0, Constants.INT_SEQ.length - 1);
+		for (int i : num) {
+			System.out.print(i);
+			if (i < num.length - 1) {
+				System.out.print(", ");
+			}
+		}
+	}
+	
+	/**
+	 * start -> end 排序
+	 * @param num
+	 * @param start
+	 * @param end
+	 */
+	public static int[] directInsertSort(int[] num, int start, int end) {
+		int tmp = 0;
+		int j = start;
+		for (int i = start; i <= end; i++) {
+			tmp = num[i];
+			// 找出tmp的位置
+			for (j = i-1; j >= start; j--) {
+				if (num[j] > tmp) {
+					// 后移>j的元素
+					num[j + 1] = num[j];
+				}
+			}
+			j = j >= 0 ? j : 0;
+			num[j] = tmp;
+		}
+		return num;
+	}
+}
---
+++ src/main/java/com/nsb/practice/algorithm/find/HillSort.java
@@ -0,0 +1,18 @@
+package com.nsb.practice.algorithm.find;
+
+/**
+ * 希尔排序
+ * @author Dorae
+ *
+ */
+public class HillSort {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	private static void hillSort() {
+		
+	}
+	
+}
