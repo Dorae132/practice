b041afc1574ade414576af8a36dd0d14e0329fe0
dorae <nsb_2017@163.com>
2019-01-28 19:10:38 +0800

    feature: lambda其实是创建了匿名内部类

--- src/main/java/com/nsb/practice/jvm/invokepkg/invokedynamic/v2/Lambda.java
+++ src/main/java/com/nsb/practice/jvm/invokepkg/invokedynamic/v2/Lambda.java
@@ -5,14 +5,30 @@ interface NsbFuncInterface {
     public boolean nsbFuncMethod(String str);
 }
 
+interface NsbFuncInterface2 {
+
+    public boolean nsbFuncMethod(String str, String str2);
+}
+
 public class Lambda {
 
     public void lambda(NsbFuncInterface func) {
         return;
     }
+
     public static void main(String[] args) {
         NsbFuncInterface nsbVar = s -> {
             int i = s.length();
-            return false;};
+            return false;
+        };
+        NsbFuncInterface nsbVar2 = s -> {
+            int i = s.length();
+            return false;
+        };
+//        NsbFuncInterface2 nsbVar2 = (s1, s2) -> {
+//            return true;
+//        };
+        System.out.println(nsbVar);
+//        System.out.println(nsbVar2);
     }
 }
